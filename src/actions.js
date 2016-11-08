afd3f3ade94e7412dba3c4d6ff74d287e01c99fe
Wicusiu <guillaume.chomat@arcan.fr>
2016-11-08 13:27:47 +0100

    Missing action

---
+++ src/actions.js
@@ -0,0 +1,24 @@
+export const SET_STATE = 'SET_STATE' ;
+export const VOTE = 'VOTE' ;
+
+export function setState(state) {
+  return {
+    type: SET_STATE,
+    state
+  };
+}
+
+export function vote(entry) {
+  return {
+    type: VOTE, 
+    meta: {remote: true},
+    entry
+  };
+}
+
+export function next() {
+  return {
+    meta: {remote: true},
+    type: 'NEXT'
+  };
+}
